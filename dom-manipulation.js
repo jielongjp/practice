25ce5b448c7fa38f61790b45e3d3ecc39f52d2e5
jielongjp <82949450+jielongjp@users.noreply.github.com>
2022-04-03 21:20:11 +0900

    Update dom-manipulation.js
    高速バスのメタタグに最安値を追加するスクリプト追加

--- dom-manipulation.js
+++ dom-manipulation.js
@@ -1,3 +1,36 @@
+//高速バストップページの最安値を取得する (最安値の配列を取得し、数値に変換して最安値をチェックする)
+let lowestPrices = []
+const el = document.querySelectorAll('.-fee.text-right.font-size-large')
+for (let i=0; i<el.length;i++) {
+  lowestPrices.push(el[i].textContent)
+}
+let pricesArr = lowestPrices.map(e => e.replace(/[^0-9]/g,'')).map(Number).filter(function(num){
+            return num != 0})
+let smallestPrice = Math.min(...pricesArr)
+//タイトルとメタディスクリプションに最安値を追加
+document.querySelector("meta[name='description']").setAttribute("content",`おすすめの格安高速バス・夜行バスを簡単に比較・予約！¥${smallestPrice}～　主要都市や地方出発、の路線を予約可能。車内Wi-fi、コンセント・USB付き、女性専用、トイレ付きなど様々な車両タイプから選べます。格安高速バス・夜行バスの予約もスカイチケット`)
+document.title = ("content",`格安高速バス・夜行バスの予約|スカイチケット ¥${smallestPrice}～`)
+
+//上記と同じですが、バス路線ページに対応(https://skyticket.jp/bus/16/30)
+let lowestPrices = []
+const el = document.querySelectorAll('.-price')
+for (let i=0; i<el.length;i++) {
+  lowestPrices.push(el[i].textContent)
+}
+let pricesArr = lowestPrices.map(e => e.replace(/[^0-9]/g,'')).map(Number).filter(function(num){
+            return num != 0})
+let smallestPrice = Math.min(...pricesArr)
+if (smallestPrice<7000){
+  const pageTitle = document.querySelector('.font-size-default-sp').textContent
+  document.querySelector("meta[name='description']").setAttribute("content",`${pageTitle}を簡単に比較・予約！¥${smallestPrice}～　主要都市や地方出発、の路線を予約可能。車内Wi-fi、コンセント・USB付き、女性専用、トイレ付きなど様々な車両タイプから選べます。格安高速バス・夜行バスの予約もスカイチケット`)
+  document.title = ("content",`${pageTitle}の予約|スカイチケット ¥${smallestPrice}～`)
+}
+else {
+  const pageTitle = document.querySelector('.font-size-default-sp').textContent
+  document.querySelector("meta[name='description']").setAttribute("content",`${pageTitle}を簡単に比較・予約！主要都市や地方出発、の路線を予約可能。車内Wi-fi、コンセント・USB付き、女性専用、トイレ付きなど様々な車両タイプから選べます。格安高速バス・夜行バスの予約もスカイチケット`)
+  document.title = ("content",`${pageTitle}の予約|スカイチケット`)
+}
+
 /*メタディスクリプションとタイトルタグ変更。yotpoのレビュー量を含める。(以前、会社レビュー用に書いたもので、現在はすでにリリースされています）
 https://adven.backlog.jp/view/GROWTH-1299#comment-137099365
 */
